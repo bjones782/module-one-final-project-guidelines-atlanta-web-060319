43b4f85f3677c6f3386b32cf41c98219dd47bf89
Dylan <dylanharris23@gmail.com>
2019-06-18 15:22:30 -0400

    First Commit

---
+++ db/migrate/20190618191711_create_questions.rb
@@ -0,0 +1,8 @@
+class CreateQuestions < ActiveRecord::Migration[5.0]
+  def change
+    create_table :questions do |t|
+      t.string :problems
+      t.string :categories
+      t.integer :points
+  end
+end
